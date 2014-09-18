2eeecb8e43e465af7e6804943445ea540eb4ec51
mrandrewmills <andrewmills@knowwwhow.com>
2014-09-18 14:32:07 -0400

    example text file and simple demo
    fixed width text file example, and demo to work with that example file.

---
+++ demo.php
@@ -0,0 +1,14 @@
+<?php
+
+	// start by including the Fixed Width File Toolkit
+	require_once 'fixedWidthFileToolkit.class.php';
+
+	// create a new instance of the class
+	$myFile = new FixedWidthFile();
+
+	// provide path and filename to readHeadersRow method
+	$myFile->readHeaderRow('arrest-9-15-2014.txt');
+
+	// and lets see our results
+	$myFile->dumpHeaders();
+?>
