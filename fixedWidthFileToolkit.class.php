18e73c3169d883e46cd2d733b233ce573b9f4740
mrandrewmills <andrewmills@knowwwhow.com>
2014-11-29 22:33:10 -0500

    Refactoring and Cleanup
    Added comments, streamlined code, making function names consistent, also
    changed scope of utility functions to private.

--- fixedWidthFileToolkit.class.php
+++ fixedWidthFileToolkit.class.php
@@ -1,14 +1,104 @@
 <?php
+/**
+ *	Fixed Width Text File Toolkit
+ *	https://github.com/mrandrewmills/Fixed-Width-Text-File-Toolkit
+ *
+ *	PHP class to help extract information from fixed width text files as easily as possible.
+ *
+ */	
 	
 	class FixedWidthFile {
+	
+		// Our properties
+	
 		private $filename;
 		private $headers;
-		private $lineLength;
+		private $lineLength = 4096; // default value, override with setFileLength() as needed
 		private $fileData;
 		
-		public function readHeaderRow($filename){
+		// ToDo: add some kind of error structure that can be accessed for last problem?
+		
+		// Our constructor function (used class name instead of __constructor to work with older versions of PHP)
+		
+		function FixedWidthFile($filename) {
+			
+			// if we received a filename
+			if ($filename) { 
+			
+				// pass it on to the setter function 
+				$this->setFilename($filename); 
+			}
+			
+		}
+		
+		// Our getters, no surprises here
+		
+		public function getFilename(){
+			return $this->filename;
+		}
+		
+		public function getHeaders(){
+			return $this->headers;
+		}
+		
+		public function getLineLength(){
+			return $this->lineLength;
+		}
 
-			$this->filename = $filename;
+		public function getfileData(){
+			return $this->fileData;
+		}
+		
+		public function getNumRows(){
+			$numRows = 0;
+			
+			if ($this->fileData) {
+				$numRows = count($this->fileData);
+			}
+			
+			return $numRows;
+		}
+
+		// Our setters, but some of our properties are not meant to be accessible (i.e. internal use only)
+
+		public function setFilename($filename){
+		
+			// verify file exists 
+			if (file_exists($filename)) {
+
+				// set the filename property accordingly
+				$this->filename = $filename;
+							
+				// attempt to read the header row
+				$this->readHeaderRow();
+				
+				// attempt to read rest of data file
+				$this->readData();
+			
+				}
+				
+			else {
+				// ToDo: error reporting that file is not valid?
+			}
+			
+			//ToDo: some kind of verification that we can access the file?
+			//ToDo: read the header row
+			//ToDo: read the data from file into an array
+		}
+		
+		public function setFileLength($fileLength) {
+		
+			// give developer means to override default fileLength value
+			
+			if (fileLength) {
+				$this->fileLength = $fileLength;
+			}
+			
+		}
+		
+		// Our "utility functions", for internal use only (thus private)
+		
+		private function readHeaderRow(){
 
 			/* check to see if file exists */
 			if (file_exists($this->filename)) {
@@ -26,41 +116,22 @@
 					// close file when we're done
 					fclose($handle);
 				}
+				else {
+					//ToDo: error handling for file permissions issue, etc.
+				}
+			}
+			else {
+				// ToDo: error handling for bad file path, typo in filename, etc.
 			}
 		}
-
-		public function dumpHeaders(){
-			
-			echo "<pre>"; 
-			echo var_dump($this->headers);
-			echo "</pre>";
-		}
 		
-		public function dumpData(){
-			
-			echo "<pre>"; 
-			// echo json_encode($this->fileData);
-			// print_r($this->fileData);
-			echo var_dump($this->fileData);
-			echo "</pre>";
-		}
-		
-		public function dumpDataJSON(){
-			
-			echo "<pre>"; 
-			echo json_encode($this->fileData);
-			echo "</pre>";
-		}
-		
-		public function getDataOut($filename){
-			
-			$this->filename = $filename; // short term shortcut
-			
+		private function readData(){
+					
 			// verify file exists, no typos, etc.
 			if (file_exists($this->filename)) {
 				
 				// open the file, suppressing any error messages with @
-				$handle = @fopen($filename, "r");
+				$handle = fopen($this->filename, "r");
 				
 				// if we were successful in opening the file
 				if ($handle) {
@@ -71,7 +142,7 @@
 					// and process the remaining rows of the file
 					while (($buffer = fgets($handle, 4096)) !== false) {
         					
-						$numFields = count($this->headers);
+        					$numFields = count($this->headers);
 												
 						// find out how long one line is
 						$fieldLength = strlen($buffer);
@@ -103,6 +174,31 @@
 			}
 			
 		}
+		
+		// Our "Conversion" functions
 
+		public function toJSON(){
+			
+			$JSONresult = json_encode($this->fileData);
+			
+			return $JSONresult;
+		}
+		
+		public function toXML(){
+			//ToDo: write code to convert PHP arrays into XML
+			$XMLresult = "";
+			
+			return $XMLresult;
+		}
+
+		// ToDo: toCSV()? 
+		public function toCSV(){
+			return true;
+		}
+		
+		// ToDo: toSQL()?
+		public function toSQL() {
+			return true;
+		}
 	}
 ?>
