ac7fbe83bfb44f34489730dce31669d2f51d4a36
Andrew Mills <andrewmills@knowwwhow.com>
2016-11-13 14:19:55 -0500

    relocate include statement

--- tests/fixedWidthFileToolkitTest.php
+++ tests/fixedWidthFileToolkitTest.php
@@ -1,7 +1,8 @@
 <?php
-class fixedWidthFileToolkitTest extends PHPUnit_Framework_TestCase{
 
   include 'fixedWidthFileToolkit.class.php';
+
+class fixedWidthFileToolkitTest extends PHPUnit_Framework_TestCase{
 
   public function testFilename(){
     $myFile = new FixedWidthFile("example.txt");
