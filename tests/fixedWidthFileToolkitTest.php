56c8145e003307c640aaac211f7a1d8569b91971
Andrew Mills <andrewmills@knowwwhow.com>
2016-11-14 08:43:23 -0500

    adding assertion to validate filename is correct

--- tests/fixedWidthFileToolkitTest.php
+++ tests/fixedWidthFileToolkitTest.php
@@ -5,7 +5,12 @@
 class fixedWidthFileToolkitTest extends PHPUnit_Framework_TestCase{
 
   public function testFilename(){
+    // create an instance of the FixedWidthFile class
     $myFile = new FixedWidthFile("example.txt");
+    // retrieve the filename value
+    $result = $myFile->getFilename();
+    // compare result vs. expectation for validation
+    $this->assertEquals("example.txt", $result);
   }
 }
 ?>
