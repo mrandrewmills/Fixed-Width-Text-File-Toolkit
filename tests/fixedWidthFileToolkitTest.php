846838e5b4fc2bb016738efb4f30e672ac88c5cf
Andrew Mills <andrewmills@knowwwhow.com>
2017-07-02 08:16:52 -0400

    remove phpunit require once
    Didn't fix issue with php7, broke php5 tests

--- tests/fixedWidthFileToolkitTest.php
+++ tests/fixedWidthFileToolkitTest.php
@@ -1,25 +1,24 @@
 <?php
 
-  require_once 'PHPUnit/Autoload.php';
   include 'fixedWidthFileToolkit.class.php';
 
-class fixedWidthFileToolkitTest extends PHPUnit_Framework_TestCase{
+  class fixedWidthFileToolkitTest extends PHPUnit_Framework_TestCase{
 
-  public function testFilename(){
-    // create an instance of the FixedWidthFile class
-    $myFile = new FixedWidthFile("example.txt");
-    // retrieve the filename value
-    $result = $myFile->getFilename();
-    // compare result vs. expectation for validation
-    $this->assertEquals("example.txt", $result);
+    public function testFilename(){
+      // create an instance of the FixedWidthFile class
+      $myFile = new FixedWidthFile("example.txt");
+      // retrieve the filename value
+      $result = $myFile->getFilename();
+      // compare result vs. expectation for validation
+      $this->assertEquals("example.txt", $result);
 
-    // check the default values for lineLength, hasHeaderRow and howStrict
-    $result = $myFile->getLineLength();
-    $this->assertEquals(4096, $result);
-    $result = $myFile->getHasHeaderRow();
-    $this->assertEquals(true, $result);
-    $result = $myFile->getHowStrict();
-    $this->assertEquals("MEDIUM", $result);
+      // check the default values for lineLength, hasHeaderRow and howStrict
+      $result = $myFile->getLineLength();
+      $this->assertEquals(4096, $result);
+      $result = $myFile->getHasHeaderRow();
+      $this->assertEquals(true, $result);
+      $result = $myFile->getHowStrict();
+      $this->assertEquals("MEDIUM", $result);
+    }
   }
-}
 ?>
